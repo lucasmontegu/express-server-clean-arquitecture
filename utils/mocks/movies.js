f329fdaf874b45faafac8c9420ffcae78556a18c
Lucas Montegú <lucas.montegu@25watts.com.ar>
2020-02-06 07:49:01 -0300

    add testing

--- utils/mocks/movies.js
+++ utils/mocks/movies.js
@@ -150,8 +150,26 @@ const moviesMock = [
             "Drama"
         ]
     }
-]
+];
+
+function filteredMoviesMock(tag) {
+    return moviesMock.filter(movie => movie.tags.includes(tag));
+}
+
+class MoviesServiceMock {
+    async getMovies() {
+        return Promise.resolve(moviesMock);
+    }
+
+    async createMovie() {
+        return Promise.resolve(moviesMock[0])
+    }
+
+
+}
 
 module.exports = {
-    moviesMock
+    moviesMock,
+    filteredMoviesMock,
+    MoviesServiceMock
 }
